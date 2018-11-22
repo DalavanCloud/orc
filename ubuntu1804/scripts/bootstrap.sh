8b7e93cb8d81e0fbd63c98710fa335060c5c7c5e
Marc van Eijk <marcvaneijk@msn.com>
2018-11-22 13:50:05 +0100

    sync

--- ubuntu1804/scripts/bootstrap.sh
+++ ubuntu1804/scripts/bootstrap.sh
@@ -1,6 +1,6 @@
-echo "============== Bootsprap script arguments ..."
-arg1=$1
-echo $arg1
+echo "============== Bootstrap script arguments ..."
+APPID=$1
+echo $APPID
 
 
 echo "============== Installing docker engine ..."
@@ -26,13 +26,19 @@ sudo apt install -y docker-ce
 echo "=========== Finished installing docker engine."
 
 # Install JQ
-sudo apt install jq
+sudo apt install -y jq
 
 echo "============== pwd ..."
 pwd
 
 echo "============== Start first container ..."
-sudo docker container run -d microsoft/azure-cli
+# -it for interactive, this will ensure the vm extension in the orchestrator will wait for the script to complete (max runtime for an extension?)
+# -e to pass in a variable. The $myvar will be available in the container 
+# files are downloaded by the vm extension to /var/lib/waagent/custom-script/download/0, this is the pwd when the agent executes the script
+# -v to map the current working directory to /src folder in the container (created in the pwd)
+# /src/sontainerlogic1.sh to run the script from the /src container (mapped from the host, downloaded by the vm extension)
+# the /src/containerlogic1.sh needs to start with #!/bin/bash
+sudo docker run -it -e "APPID=$APPID" -v $(pwd):/src microsoft/azure-cli /src/containerlogic1.sh
 
 
 
